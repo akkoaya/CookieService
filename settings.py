17ef0ee01d6027111c9b5502ff5d3f53be8c479a
akkoaya <151345394+akkoaya@users.noreply.github.com>
2023-12-31 15:04:51 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 # 网站账号管理
 Accounts = {
     "zhihu":{
-        "username":"18545531240",
-        "password":"admin123456",
+        "username":"****",
+        "password":"****",
         "cookie_name":"zhihu_cookies", #reids里的set的名字
         "max_cookie_num":1, #可以根据网站调节，cookie池的大小
         "check_cookie_interval":30 #检擦cookie有效的时间间隔
